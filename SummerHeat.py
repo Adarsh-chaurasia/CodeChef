643ff25a62260c491f055f52e0ba69b981a4264f
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-11 08:00:34 +0100

    Create SummerHeat.py

---
+++ SummerHeat.py
@@ -0,0 +1,12 @@
+#AdarshChaurasia
+
+def main():
+    for _ in range(int(input())):
+        a,b,A,B=map(int,input().split())
+        res=A//a+B//b
+        
+        
+        print(res)
+        
+        
+main()
